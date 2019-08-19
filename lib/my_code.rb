a2ff4de9b9196786aef882afca2ba9d0d0e4b8d3
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 23:34:15 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  source_array.length.times do |index|
+    new_value = source_array[index] * -1
+    source_array[index] = new_value
+  end
+  
+  source_array
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  source_array.length.times do |index|
+    new_value = source_array[index] * 2
+    source_array[index] = new_value
+  end
+  source_array
+end
+
+def map_to_square(source_array)
+  source_array.length.times do |index|
+    new_value = source_array[index] * source_array[index]
+    source_array[index] = new_value
+  end
+  source_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  new_value = starting_point
+  # source_array.length.times do |index|
+  #   if source_array[index + starting_point]
+  #   new_value += source_array[index + starting_point] 
+  #   end
+  # end
+  # new_value
+  
+  for i in (source_array)
+  new_value += i
+  end
+  return new_value
+end
+
+def reduce_to_all_true(source_array)
+  for i in (source_array)
+  
+    if i == !true
+      return false
+    end
+  end
+return true
+end
+
+def reduce_to_any_true(source_array)
+  for i in (source_array)
+  
+    if i == true
+      return true
+    end
+  end
+return false
+end
+
+
+
+
+
+
+
