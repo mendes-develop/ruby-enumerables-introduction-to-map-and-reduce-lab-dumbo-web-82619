f7c324be52fe95af386635cc8a1c0f0de95a3506
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 22:41:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 # My Code here....
 
 def map_to_negativize(source_array)
-  source_array.lenght.times do |index|
+  source_array.length.times do |index|
     new_value = source_array[index] * -1
     source_array[index] = new_value
   end
