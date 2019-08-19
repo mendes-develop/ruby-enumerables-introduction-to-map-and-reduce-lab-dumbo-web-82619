f9fedf6c9d35f8a2b917db953455acc344ebd30e
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 23:24:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,8 +48,9 @@ def reduce_to_all_true(source_array)
   for i in (source_array)
     if i == false
       return false
+  
   end
-  return true
+  # return true
 end
 
 def reduce_to_any_true(source_array)
