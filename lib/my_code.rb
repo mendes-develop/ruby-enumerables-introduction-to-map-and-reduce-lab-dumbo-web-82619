94dee07ecea65ebc2305eab3a6e4f85d431d14f8
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 22:45:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,19 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  source_array.length.times do |index|
+    new_value = source_array[index] * 2
+    source_array[index] = new_value
+  end
+  source_array
+end
+
+def map_to_square(source_array)
+  source_array.length.times do |index|
+    new_value = source_array[index] * source_array[index]
+    source_array[index] = new_value
+  end
+  source_array
+end
